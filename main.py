74883fe3d5f961663194e581fd03060b8e53d73c
matheesha-pathirana <pathrianamatheesha@gmail.com>
2022-04-23 13:54:46 +0530

    updates

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import random
 import time
 import pyautogui
 
-number = int(input('How many messages do you want to send? :'))
+message=str(input('Enter the message you want to Send : '))
+number = int(input('How many messages do you want to Send? :'))
+number+=1
 time.sleep(5)  # Delay
-a = 0
+a = 1
 while True:
-    with open("text.txt", "r") as file:  # Open the file in read mode
-        allText = file.read()
-        words = list(map(str, allText.split()))
-
-        pyautogui.typewrite((random.choice(words)))
+        pyautogui.typewrite(message)
         pyautogui.press("enter")
         print(f'Successfully send the message {a}')
-
-    a += 1
-    if a == number:  # Automatically exits after sending messages
-        exit()
+        a += 1
+        if a == number:  # Automatically exits after sending messages
+            exit()
